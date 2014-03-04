6622df2cf239fe81c1eaf55c76f3b2702f168303
Clinton Strong <clintxs@gmail.com>
2014-03-04 05:32:51 -0600

    Initial Commit

---
+++ SetupIconsForTheme/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  SetupIconsForTheme
+//
+//  Created by Clinton Strong on 3/4/14.
+//  Copyright (c) 2014 Clinton Strong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
